4f3da8f852511e1bec634425370f56bf9989c818
minseok baek <white 0825@ajou.ac.kr>
2023-06-01 21:15:34 +0900

    HomeActivity view binding updated

--- app/src/main/java/com/ajoudb/ajouwiki/activity/HomeActivity.kt
+++ app/src/main/java/com/ajoudb/ajouwiki/activity/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.ajoudb.ajouwiki.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ajoudb.ajouwiki.R
+import com.ajoudb.ajouwiki.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityHomeBinding?= null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        mBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

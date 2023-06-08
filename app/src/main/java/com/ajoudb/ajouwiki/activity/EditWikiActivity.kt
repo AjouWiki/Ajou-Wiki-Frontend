e693942dfef1cb98f680c18c3f5792322e5a5e13
minseok baek <white 0825@ajou.ac.kr>
2023-06-08 20:21:15 +0900

    View Binding 연결

--- app/src/main/java/com/ajoudb/ajouwiki/activity/EditWikiActivity.kt
+++ app/src/main/java/com/ajoudb/ajouwiki/activity/EditWikiActivity.kt
@@ -2,11 +2,29 @@ package com.ajoudb.ajouwiki.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ajoudb.ajouwiki.R
+import android.util.Log
+import com.ajoudb.ajouwiki.databinding.ActivityEditWikiBinding
 
 class EditWikiActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityEditWikiBinding?= null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_wiki)
+        mBinding = ActivityEditWikiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val markdownEditText = binding.editText
+        val stylesBar = binding.stylesBar
+
+        markdownEditText.setStylesBar(stylesBar)
+
+        binding.complete.setOnClickListener {
+            //마크다운 잘 넘어감 (bold, italic은 안됨;;)
+            Log.d("check", binding.editText.getMD())
+        }
+
     }
 }

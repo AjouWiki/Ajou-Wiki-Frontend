0650859a113c75257fd6001e49819101eb3e4606
K-osA <evbayooooo@gmail.com>
2023-06-08 20:40:13 +0900

    manymany

--- app/src/main/java/com/ajoudb/ajouwiki/Tag.kt
+++ app/src/main/java/com/ajoudb/ajouwiki/Tag.kt
@@ -3,9 +3,6 @@ package com.ajoudb.ajouwiki
 import java.io.Serializable
 
 data class Tag (
-    var name: String?,
-    var student_id: String?,
-    var email: String?,
-    var department: String?,
-    var sex: String?
+    var id: Int?,
+    var name: String?
 ):Serializable

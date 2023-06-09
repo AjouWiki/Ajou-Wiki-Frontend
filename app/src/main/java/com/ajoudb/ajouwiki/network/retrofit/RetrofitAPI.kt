773b26aa1682328c346bbc3d0cb637cee782a35e
K-osA <evbayooooo@gmail.com>
2023-06-09 11:53:36 +0900

    prod url

--- app/src/main/java/com/ajoudb/ajouwiki/network/retrofit/RetrofitAPI.kt
+++ app/src/main/java/com/ajoudb/ajouwiki/network/retrofit/RetrofitAPI.kt
@@ -17,7 +17,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitAPI {
-    private const val BASE_URL = "http://10.0.2.2:8000/api/v1/"
+    private const val BASE_URL = "https://ajouwiki.onrender.com/api/v1/"
 
     private val okHttpClient: OkHttpClient by lazy {
         OkHttpClient.Builder()
